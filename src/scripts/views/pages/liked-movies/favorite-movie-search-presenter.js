70001cb59579e19651c21475cff484c8841ab2f8
Fikri <muhamadfikri64@yahoo.co.id>
2021-06-22 06:33:13 +0700

    next test search favorite movies

--- src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -11,8 +11,11 @@ class FavoriteMovieSearchPresenter {
     });
   }
 
-  _searchMovies(latestQuery) {
+  async _searchMovies(latestQuery) {
     this._latestQuery = latestQuery;
+
+    const foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+
     this._favoriteMovies.searchMovies(this.latestQuery);
   }
 
@@ -27,6 +30,9 @@ class FavoriteMovieSearchPresenter {
     );
 
     document.querySelector('.movies').innerHTML = html;
+
+    document.getElementById('movie-search-container')
+    .dispatchEvent(new Event('movies:searched:updated'));
   }
 }
 
